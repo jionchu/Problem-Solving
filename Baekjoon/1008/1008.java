f5826d752ecac6956d2b29d6bc9abc21525e525f
Jion Chu <40855422+jionchu@users.noreply.github.com>
2020-01-16 00:02:38 +0900

    Create 1008.java

---
+++ Baekjoon/1008/1008.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] ar){
+		Scanner keyboard = new Scanner(System.in);
+		int A = keyboard.nextInt();
+		int B = keyboard.nextInt();
+		System.out.println((double)A/B);
+	}
+}
