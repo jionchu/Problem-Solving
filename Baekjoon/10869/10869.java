77a401e60fb4831062167a7e018acf119c072b69
Jion Chu <40855422+jionchu@users.noreply.github.com>
2020-01-17 21:17:26 +0900

    Create 10869.java

---
+++ Baekjoon/10869/10869.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] ar){
+		Scanner keyboard = new Scanner(System.in);
+		int A = keyboard.nextInt();
+		int B = keyboard.nextInt();
+		System.out.println(A+B);
+		System.out.println(A-B);
+		System.out.println(A*B);
+		System.out.println(A/B);
+		System.out.println(A%B);
+	}
+}
