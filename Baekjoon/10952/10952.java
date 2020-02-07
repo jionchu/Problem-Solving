9e36132c36620e322a8f93a0a432d5389fd1d5c3
Jion Chu <40855422+jionchu@users.noreply.github.com>
2020-02-07 10:42:31 +0900

    BOJ 10952

---
+++ Baekjoon/10952/10952.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/*
+Baekjoon Online Judge
+10952: A+B - 5
+*/
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+		
+    while(true) {
+      int A = sc.nextInt();
+      int B = sc.nextInt();
+			
+      if(A==0 && B==0)
+        break;
+      else
+        System.out.println(A+B);
+    }
+  }
+}
