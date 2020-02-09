c309f9775515641f5b45f9c04603f9c64679b127
Jion Chu <40855422+jionchu@users.noreply.github.com>
2020-02-09 15:43:00 +0900

    EPPER 14-1

---
+++ E-PPER/14회/Q1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/* E-PPER
+ * Q1: 윤년 판별하기
+ * created at 2020.02.09
+ * created by jionchu */
+
+public class Q1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int y = sc.nextInt();
+		
+		if((y%4==0&&y%100!=0)||y%400==0) // 윤년일 때
+			System.out.println("T");
+		else System.out.println("F"); // 윤년이 아닐 때
+	}
+}
