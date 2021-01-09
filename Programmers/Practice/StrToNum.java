1a0768a4fa7598818653ec0fd821aa88ce4133ff
jionchu <fn0809@gmail.com>
2021-01-10 01:40:50 +0900

    Programmers 연습문제 문자열을 정수로 바꾸기

---
+++ Programmers/Practice/StrToNum.java
@@ -0,0 +1,10 @@
+/* Programmers
+ * 연습문제 - 문자열을 정수로 바꾸기
+ * created on 2021.01.10
+ * created by jionchu */
+
+class Solution {
+    public int solution(String s) {
+        return Integer.parseInt(s);
+    }
+}
