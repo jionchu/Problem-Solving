6ed7c705bca0ef23ad967258dabe221c981adaad
jionchu <fn0809@gmail.com>
2021-01-17 11:58:34 +0900

    Programmers 연습문제 짝수와 홀수

---
+++ Programmers/Practice/EvenOdd.java
@@ -0,0 +1,10 @@
+/* Programmers
+ * 연습문제 - 짝수와 홀수
+ * created on 2021.01.17
+ * created by jionchu */
+
+class Solution {
+    public String solution(int num) {
+        return num % 2 == 0 ? "Even" : "Odd";
+    }
+}
