eaad448e8d7651655adf2356bace24efe115171b
jionchu <fn0809@gmail.com>
2021-01-24 12:39:35 +0900

    Programmers 행렬의 덧셈

---
+++ Programmers/Practice/SumMatrix.java
@@ -0,0 +1,16 @@
+/* Programmers
+ * 연습문제 - 행렬의 덧셈
+ * created on 2021.01.24
+ * created by jionchu */
+
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr1[0].length];
+        for (int i=0;i<arr1.length;i++) {
+            for (int j=0;j<arr1[0].length;j++) {
+                answer[i][j] = arr1[i][j]+arr2[i][j];
+            }
+        }
+        return answer;
+    }
+}
