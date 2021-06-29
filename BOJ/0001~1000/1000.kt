9dd091b7cfaf9144aef2ba3405babbbca177f203
jionchu <fn0809@gmail.com>
2021-06-29 21:35:05 +0900

    BOJ 1000

---
+++ BOJ/0001~1000/1000.kt
@@ -0,0 +1,16 @@
+/* Baekjoon Online Judge
+ * 1000: A+B
+ * created on 2021.06.29
+ * created by jionchu */
+
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+
+    val sc: Scanner = Scanner(System.`in`)
+
+    var a = sc.nextInt()
+    var b = sc.nextInt()
+    
+    println(a+b)
+}
