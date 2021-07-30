99aa9ad81681298d6a5d0fedf2938c35788d53fd
jionchu <fn0809@gmail.com>
2021-07-31 00:22:32 +0900

    Programmers 징검다리 건너기

---
+++ Programmers/2019 카카오 개발자 겨울 인턴십/징검다리 건너기.kt
@@ -0,0 +1,59 @@
+/* Programmers
+ * 2019 카카오 개발자 겨울 인턴십 - 징검다리 건너기
+ * created on 2021.07.31
+ * created by jionchu */
+
+class Solution {
+    
+    var limit = 0
+    var answer = 0
+    
+    fun solution(stones: IntArray, k: Int): Int {
+        
+        limit = k
+        findAnswer(stones, stones.min()!!, stones.max()!!)
+        
+        return answer
+    }
+
+    // 이분탐색
+    fun findAnswer(stones: IntArray, min: Int, max: Int) {
+        if (min != max) {
+
+            val middle = min + (max-min)/2
+
+            if (isCrossingable(stones, middle)) {
+                findAnswer(stones, middle+1, max)
+            } else {
+                findAnswer(stones, min, middle)
+            }
+
+        } else {
+            isCrossingable(stones,min)
+        }
+    }
+
+    // num 명의 사람이 징검다리를 건널 수 있는지 확인
+    fun isCrossingable(stones: IntArray, num: Int): Boolean {
+        var i = limit-1
+        var prev = 0
+
+        while (i < stones.size) {
+
+            prev = i-limit
+            
+            // 디딤돌을 건너지 못하는 경우
+            while (stones[i] < num) {
+                i--
+                if (prev == i) return false
+            }
+
+            // 다음 디딤돌 확인하기
+            i += limit
+        }
+
+        answer = num
+        // 마지막 디딤돌까지 무사히 건넌 경우
+        return true
+    }
+}
