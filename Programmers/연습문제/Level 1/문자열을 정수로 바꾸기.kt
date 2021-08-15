650856899058da99bb9a7a92a57461496341652e
jionchu <fn0809@gmail.com>
2021-08-15 19:41:09 +0900

    Programmers 문자열을 정수로 바꾸기

---
+++ Programmers/연습문제/Level 1/문자열을 정수로 바꾸기.kt
@@ -0,0 +1,10 @@
+/* Programmers
+ * 연습문제 - 문자열을 정수로 바꾸기
+ * created on 2021.08.15
+ * created by jionchu */
+
+class Solution {
+    fun solution(s: String): Int {
+        return s.toInt()
+    }
+}
